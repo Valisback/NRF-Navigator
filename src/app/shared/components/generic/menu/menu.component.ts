4798a827008814215c9a116f32e8282772e0a528
Valentin <49312215+Vale-polito@users.noreply.github.com>
2020-01-06 13:09:47 -0500

    Fixing side-menu bug

--- src/app/shared/components/generic/menu/menu.component.ts
+++ src/app/shared/components/generic/menu/menu.component.ts
@@ -27,7 +27,7 @@ export class MenuComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.disableSwipe();
+    //this.disableSwipe();
     this.storageService.allCompanies.subscribe((cpies) => {
       this.allCompanies = cpies;
     });
